b0f8e835da8ed85cc8fc65ae2a7d78de508b90a7
M-Melodious <jemishmavani@outlook.com>
2022-04-17 19:38:39 +0530

    changed dockerfile

--- yolox_x/run.sh
+++ yolox_x/run.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-source /opt/intel/openvino/bin/setupvars.sh && python3 multi_channel_yolox.py
+#source /opt/intel/openvino/bin/setupvars.sh && python3 multi_channel_yolox.py
+echo "Running inference with OpenVINO"
+python3 multi_channel_yolox.py
