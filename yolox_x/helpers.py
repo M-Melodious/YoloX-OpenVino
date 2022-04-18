2c560b79095ec390dc4b28a32bc24ebb5b59ab3c
M-Melodious <jemishmavani@outlook.com>
2022-04-18 14:40:37 +0530

    refactor the code

---
+++ yolox_x/helpers.py
@@ -0,0 +1,177 @@
+import cv2
+import numpy as np
+
+from filevideostream import VideoCap
+from yolox.preprocess import preproc_buffer
+from yolox.visualize import draw_bbox, concat_tile
+from yolox.utils import multiclass_nms, demo_postprocess
+
+
+def get_video_caps(video_list:list, resize:tuple=None,
+                   buf_size:int=64) -> list:
+    """
+    Helper function to create a VideoCap thread with processing
+    for every video source.
+
+    Parameters
+    ----------
+        video_list: list
+            list of video source paths
+        resize: tuple
+            resize tuple for preprocessing, default: (640, 640)
+            Note: default value is based on the network used.
+        buf_size: int
+            buffer size to store the decoded frames in background
+            Default: 64
+
+    Returns
+    -------
+        video_caps list
+    
+    """
+    video_caps = []
+    preproc_args = {'input_size': resize if resize else (640,640),
+                    'swap': (2,0,1)
+                    }
+
+    for vid in video_list:
+        video_cap = VideoCap(vid, preproc=preproc_buffer,
+                             preproc_args=preproc_args,
+                             buf_size=buf_size)
+
+        video_caps.append(video_cap)
+
+    return video_caps
+
+
+def get_video_writer(out_name:str, fps:int=30, size:tuple=None):
+    """
+    Helper function to create video writer.
+    
+    """
+    assert size, "Size needed for video writer"
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    return cv2.VideoWriter(out_name, fourcc, fps, size)
+
+
+def stop_video_caps(vid_caps:list):
+    """
+    Helper function to stop the video cap thread in background
+    
+    """
+    for vid in vid_caps:
+        vid.fvs.stop()
+
+
+def batchify_frames(frames:list) -> np.ndarray:
+    """
+    Helper function to batchify the given list of frames
+
+    Parameters
+    ----------
+        frames: list
+            list of preprocessed frames
+
+    Returns
+    -------
+        np array
+        
+    """
+    return np.array(frames)
+
+
+def get_detections(video_caps:list, output:np.ndarray,
+                   net_shape:tuple=(640,640), nms_thresh:float=0.45,
+                   score_thresh:float=0.1) -> None:
+    """
+    Helper function to post process the detections.
+
+    Parameters
+    ----------
+        video_caps: list
+            list of video caps objects
+        output: np.ndarray
+            output from inference net
+        net_shape: tuple
+            preprocess shape, default: (640, 640)
+            Note: it is based on the net used
+        nms_thresh: float
+            nms threshold, default: 0.45
+        score_thresh: float
+            score thresh, default: 0.1
+
+    """
+    predictions = demo_postprocess(output, net_shape)
+
+    ## Loop through batch predictions and get detections
+    for i in range(len(predictions)):
+        preds = predictions[i].copy()
+        boxes = preds[:, :4]
+        scores = preds[:, 4, None] * preds[:, 5:]
+
+        boxes_xyxy = np.ones_like(boxes)
+        boxes_xyxy[:, 0] = boxes[:, 0] - boxes[:, 2]/2.
+        boxes_xyxy[:, 1] = boxes[:, 1] - boxes[:, 3]/2.
+        boxes_xyxy[:, 2] = boxes[:, 0] + boxes[:, 2]/2.
+        boxes_xyxy[:, 3] = boxes[:, 1] + boxes[:, 3]/2.
+        boxes_xyxy /= video_caps[i].ratio
+
+        video_caps[i].dets = multiclass_nms(boxes_xyxy, scores,
+                                            nms_thr=nms_thresh,
+                                            score_thr=score_thresh)
+
+
+def put_highlighted_text(frame:np.ndarray, message:str, position:tuple,
+                         font_face:int, font_scale:float, color:tuple,
+                         thickness:int) -> None:
+    """
+    Helper function to put the text on the image.
+    
+    """
+##    cv2.putText(frame, message, position, font_face, font_scale,
+##                (255, 255, 255), thickness + 1) # white border
+    cv2.putText(frame, message, position, font_face, font_scale, color, thickness)
+    
+
+def visualize_multicam_detections(frames:list, video_caps:list,
+                                  max_window_size:tuple=(1280, 1040),
+                                  score_thr:float=0.1) -> np.ndarray:
+    """
+    Helper function to draw bboxes over images and make grid out of
+    image batch.
+
+    Parameters
+    ----------
+        frames: list
+            list of frames
+        video_caps: list
+            list of video caps object
+        max_window_size: tuple
+            max size for grid, default: (1280, 1040)
+        score_thr: float
+            score threshold, default: 0.1
+
+    Returns
+    -------
+        grid image
+        
+    """
+    vis = None
+    default_size = (640, 480)
+    tile_frames = []
+    for i, vid in enumerate(video_caps):
+        if vid.dets is not None:
+            final_boxes = vid.dets[:, :4]
+            final_scores, final_cls_inds = vid.dets[:, 4], vid.dets[:, 5]
+            draw_bbox(frames[i][0], final_boxes, final_scores,
+                      final_cls_inds, conf=score_thr)
+            tile_frames.append(cv2.resize(frames[i][0], default_size))
+
+    grid = concat_tile([tile_frames[:2], tile_frames[2:]])
+    if (grid.shape[0] > max_window_size[1]
+        or grid.shape[1] > max_window_size[0]):
+        grid = cv2.resize(grid, max_window_size)
+
+    del tile_frames
+
+    return grid
--- yolox_x/multi_channel_yolox.py
+++ yolox_x/multi_channel_yolox.py
@@ -8,150 +8,61 @@ import logging as log
 
 from pathlib import Path
 
-from yolox.visualize import draw_bbox, concat_tile
-from yolox.preprocess import preproc_buffer
-from yolox.coco_classes import COCO_CLASSES
-from yolox.utils import mkdir, multiclass_nms, demo_postprocess
+from yolox.utils import mkdir
 
 from inference import Network
 from filevideostream import VideoCap, ThreadedGenerator
 from config import inference, vid_sources, post_process, visualization
 
-
-
-def get_video_caps(video_list, resize=None, buf_size=64):
-    video_caps = []
-    preproc_args = {'input_size': resize if resize else (640,640),
-                    'swap': (2,0,1)
-                    }
-
-    for vid in video_list:
-        video_cap = VideoCap(vid, preproc=preproc_buffer,
-                             preproc_args=preproc_args,
-                             buf_size=buf_size)
-
-        video_caps.append(video_cap)
-
-    return video_caps
-
-def get_video_writer(out_name, fps=30, size=None):
-    assert size, "Size needed for video writer"
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    return cv2.VideoWriter(out_name, fourcc, fps, size)
-
-def stop_video_caps(vid_caps):
-    for vid in vid_caps:
-        vid.fvs.stop()
-
-def batchify_frames(frames):
-    return np.array(frames)
-
-def get_detections(video_caps, output, net_shape=(640,640),
-                   nms_thresh=0.45, score_thresh=0.1):
-    predictions = demo_postprocess(output, net_shape)
-
-    for i in range(len(predictions)):
-        preds = predictions[i].copy()
-        boxes = preds[:, :4]
-        scores = preds[:, 4, None] * preds[:, 5:]
-
-        boxes_xyxy = np.ones_like(boxes)
-        boxes_xyxy[:, 0] = boxes[:, 0] - boxes[:, 2]/2.
-        boxes_xyxy[:, 1] = boxes[:, 1] - boxes[:, 3]/2.
-        boxes_xyxy[:, 2] = boxes[:, 0] + boxes[:, 2]/2.
-        boxes_xyxy[:, 3] = boxes[:, 1] + boxes[:, 3]/2.
-        boxes_xyxy /= video_caps[i].ratio
-
-        video_caps[i].dets = multiclass_nms(boxes_xyxy, scores,
-                                            nms_thr=nms_thresh,
-                                            score_thr=score_thresh)
-
-##def get_target_size(frame_sizes, vis=None,
-##                    max_window_size=(1920, 1080), stack_frames='vertical'):
-##    if vis is None:
-##        width = 0
-##        height = 0
-##        
-##        for size in frame_sizes:
-##            if width > 0 and height > 0:
-##                if stack_frames == 'vertical':
-##                    height += size[1]
-##                elif stack_frames == 'horizontal':
-##                    width += size[0]
-##            else:
-##                width, height = size
-##    else:
-##        height, width = vis.shape[:2]
-##
-##    if stack_frames == 'vertical':
-##        target_height = max_window_size[1]
-##        target_ratio = target_height / height
-##        target_width = int(width * target_ratio)
-##        
-##    elif stack_frames == 'horizontal':
-##        target_width = max_window_size[0]
-##        target_ratio = target_width / width
-##        target_height = int(height * target_ratio)
-##        
-##    return target_width, target_height
-
-def put_highlighted_text(frame, message, position,
-                         font_face, font_scale, color, thickness):
-    cv2.putText(frame, message, position, font_face, font_scale,
-                (255, 255, 255), thickness + 1) # white border
-    cv2.putText(frame, message, position, font_face, font_scale, color, thickness)
-
-def visualize_multicam_detections(frames, video_caps,
-                                  max_window_size=(1280, 1040), score_thr=0.1):
-    vis = None
-    default_size = (640, 480)
-    tile_frames = []
-    for i, vid in enumerate(video_caps):
-        if vid.dets is not None:
-            final_boxes = vid.dets[:, :4]
-            final_scores, final_cls_inds = vid.dets[:, 4], vid.dets[:, 5]
-            draw_bbox(frames[i][0], final_boxes, final_scores,
-                      final_cls_inds, conf=score_thr)
-            tile_frames.append(cv2.resize(frames[i][0], default_size))
-
-    grid = concat_tile([tile_frames[:2], tile_frames[2:]])
-    if (grid.shape[0] > max_window_size[1]
-        or grid.shape[1] > max_window_size[0]):
-        grid = cv2.resize(grid, max_window_size)
-
-    del tile_frames
-
-    return grid
+from helpers import (
+    get_video_caps,
+    get_video_writer,
+    stop_video_caps,
+    batchify_frames,
+    get_detections,
+    put_highlighted_text,
+    visualize_multicam_detections
+)
     
 
 def _run_inference_async(infer_net, threaded_gen, video_caps, video_writer):
+    """
+    Function for async inference.
+    
+    """
     cur_batch_id = 0
     next_batch_id = 1
     
     _, c, h, w = infer_net.get_input_shape()
     start_time = time.time()
 
+    ## Get the fist batch and perform inference
     prev_frames = next(threaded_gen)
     infer_net.exec_net_async(cur_batch_id,
                              batchify_frames([frame[1] for frame in prev_frames]))
 
+    ## Loop through the batches and inference in async mode
     for frames in threaded_gen:
         
         images = batchify_frames([frame[1] for frame in frames])
         infer_net.exec_net_async(next_batch_id, images)
 
+        ## If previous inference is done grab the output and post process it
         if infer_net.wait(cur_batch_id) == 0:
             res = infer_net.get_output_async(cur_batch_id)
             get_detections(video_caps, res, net_shape=(h, w),
                            nms_thresh=post_process.nms_thr,
                            score_thresh=post_process.score_thr)
 
+        ## Swap batch ids
         cur_batch_id, next_batch_id = next_batch_id, cur_batch_id
 
+        ## Draw bboxes and make grid out of the batch
         vis = visualize_multicam_detections(prev_frames, video_caps,
                                             max_window_size=visualization.max_window_size,
                                             score_thr=post_process.score_thr)
 
+        ## Statistics
         fps_time = time.time() - start_time
         fps_message = f"FPS: {1/fps_time:.3f}"
         inf_time_message = f"Inference time: N/A for async mode"
@@ -160,34 +71,45 @@ def _run_inference_async(infer_net, threaded_gen, video_caps, video_writer):
         put_highlighted_text(vis, inf_time_message, (15, 60),
                              cv2.FONT_HERSHEY_COMPLEX, 0.75, (200, 10, 10), 2)
 
+        ## Write frame to output video
         video_writer.write(vis)
 
         prev_frames, frames = frames, prev_frames
 
+    ## Stop the video threads
     stop_video_caps(video_caps)
+    
 
 def _run_inference_sync(infer_net, threaded_gen, video_caps, video_writer):
+    """
+    Function for sync inference.
+    
+    """
     _, c, h, w = infer_net.get_input_shape()
     det_time = 0
     
     start_time = time.time()
 
+    ## Loop through batches and perform inference
     for frames in threaded_gen:
 
         det_start = time.time()
         images = batchify_frames([frame[1] for frame in frames])
         infer_net.exec_net(images)
         det_time = time.time() - det_start
-        
+
+        ## Get output and post process it
         res = infer_net.get_output()
         get_detections(video_caps, res, net_shape=(h, w),
                        nms_thresh=post_process.nms_thr,
                        score_thresh=post_process.score_thr)
 
+        ## Draw bboxes and make grid out of the batch
         vis = visualize_multicam_detections(frames, video_caps,
                                             max_window_size=visualization.max_window_size,
                                             score_thr=post_process.score_thr)
 
+        ## Statistics
         fps_time = time.time() - start_time
         fps_message = f"FPS: {1/fps_time:.3f}"
         inf_time_message = f"Inference time: {det_time*1000:.3f}, Sync Mode"
@@ -197,37 +119,50 @@ def _run_inference_sync(infer_net, threaded_gen, video_caps, video_writer):
                              cv2.FONT_HERSHEY_COMPLEX, 0.75, (200, 10, 10), 2)
 
 
-        video_writer.write(vis)
+        ## Write grid frame to output video
+        cv2.imwrite("temp.jpg", vis)
+##        video_writer.write(vis)
 
+    ## Stop all video threads
     stop_video_caps(video_caps)
 
         
 
 def main():
-
+    """
+    Main entry point function.
+    
+    """
+    ## Create network and load the weights
     infer_net = Network()
     infer_net.load_model(inference.model, inference.device,
                          num_requests=inference.num_requests,
                          batch_size=inference.batch_size)
     _, c, h, w = infer_net.get_input_shape()
 
+    ## Create video threads and start the threads to fill up the buffer in background
     videos_list = list(Path(vid_sources.source_dir).glob("*.*"))
     video_caps = get_video_caps([vid.as_posix() for vid in videos_list],
                                 resize=(h,w), buf_size=32)
 
     threaded_gen = ThreadedGenerator(video_caps).__iter__()
 
+    ## Create video writer
     mkdir(visualization.output_dir)
     out_video_name = Path(visualization.output_dir, "4-channel-yolox-x.mp4").as_posix()
     video_writer = get_video_writer(out_video_name, size=visualization.target_size)
 
+    ## Perform inference according to mode specify in config file
     if inference.mode == 'async':
+        print(f"Inference in async mode")
         _run_inference_async(infer_net, threaded_gen, video_caps, video_writer)
     elif inference.mode == 'sync':
+        print(f"Inference in sync mode")
         _run_inference_sync(infer_net, threaded_gen, video_caps, video_writer)
     else:
         print(f"Unknown mode!!! Please either use async or sync")
 
+    ## Release the object
     video_writer.release()
     print("Done!!!")
 
